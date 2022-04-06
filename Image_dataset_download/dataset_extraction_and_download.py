4c0da52c82c11c77f5d555af1e96a47f30ac5a95
andrlikp <72546103+andrlikp@users.noreply.github.com>
2022-04-06 20:33:12 +0200

    Add files via upload

--- Image_dataset_download/dataset_extraction_and_download.py
+++ Image_dataset_download/dataset_extraction_and_download.py
@@ -12,13 +12,17 @@ from IPython.display import clear_output
 from threading import Thread
 
 errors = 0
+downloaded = 0
 
 def save_img(row,image_name):
     global errors
+    global downloaded
     try:
         img_data = requests.get(row).content
         with open('downloaded/'+image_name+'.jpg', 'wb') as handler:
             handler.write(img_data)
+        downloaded+=1
+        print('downloaded: '+dowloaded)
     except:
         errors+=1
 
@@ -41,19 +45,13 @@ def main():
         datasets[doc] = pd.concat(subsets, axis=0, ignore_index=True)
         
     for row in datasets['photos']['photo_image_url']:
-        #if i>2000: break
-        #if len(row)<3: continue
-        #print(row[0].split(',')[0])
         if i<0: #jen podmínka pro ty co už mám stažené, stačí i=0 když není potřeba
             i+=1
             continue
         image_name = datasets['photos']['photo_id'][i]
         Thread(target=save_img, args=(row, image_name)).start()
-        clear_output()
-        print('saved', " ", i)
         i+=1
         if i>6000+errors:break #zastavovací podmínka
-    print('all saved')
     
 if __name__ == '__main__':
     main()
